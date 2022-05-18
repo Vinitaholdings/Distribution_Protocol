5a867946ab3e08e278af73612dc91e78b9e8cab7
blok-hamster <josicoded@gmail.com>
2022-05-18 08:35:23 -0700

    refactor

--- contracts/Users/Artiste.sol
+++ contracts/Users/Artiste.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../Products/CroudFundNonRefundable.sol";
-import "../Products/CroudFundRefundable.sol";
-import "../Tokens/PicardyArtisteToken.sol";
-*/ 
+
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "../Products/CroudFundNonRefundable.sol";
+//import "../Products/CroudFundRefundable.sol";
+//import "../Tokens/PicardyArtisteToken.sol";
+
 
 contract PicardyArtiste is Ownable {
 
@@ -27,8 +27,8 @@ contract PicardyArtiste is Ownable {
     }
 
     function addProduct(uint _productId, address _productAddress) external onlyOwner {
-        require(!activeProduct);
-        address[] memory newProduct = productMap[_productId];
+        require(!activeProduct[_productId]);
+        address[] storage newProduct = productMap[_productId];
         newProduct.push(_productAddress);
         products.push(_productAddress);
         activeProduct[_productId] = true;
